41391b1cafc14fd22aa7afee2d7863d50d65a903
Documenter.jl <documenter@juliadocs.github.io>
2022-04-20 23:22:29 +0000

    build based on dd05d68

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Mimir","category":"page"},{"location":"#Mimir","page":"Home","title":"Mimir","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Mimir.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Mimir]","category":"page"}]
+}
